b95330a8cb8489f0a27e2e25bbebe72b7a551974
rachelkathleen <rachel.hawa@gmail.com>
2019-05-20 20:22:42 -0500

    ass welcome and home

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-  root 'sessions#home'
+  root 'sessions#welcome'
+  root 'sessions#goodbye'
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+  get '/home' => 'users#home'
   resources :wines
   resources :users
 end
