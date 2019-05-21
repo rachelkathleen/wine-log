ca3850b4408f94b92e2e144d0554d6f58011efc8
rachelkathleen <rachel.hawa@gmail.com>
2019-05-20 20:21:16 -0500

    update create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  #signup
   def create
-    @user = User.new(user_params)
-    if @user.save
-      #login the user
-      session[:user_id] = @user.id
-      redirect_to '/wines'
-    else
-      render :new
+    @user = User.create(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.id
+    redirect_to '/home'
+  end
+
+  def home
+    @user = current_user
+    unless logged_in?
+      redirect_to '/welcome'
     end
   end
 
@@ -20,7 +22,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:user_name, :first_name, :last_name, :email, :password)
+    params.require(:user).permit(:user_name, :first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end
