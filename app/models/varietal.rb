41f004951687611f9a09994598e6869cf4a5b03b
rachelkathleen <rachel.hawa@gmail.com>
2019-05-22 13:40:04 -0500

    add has many countries through wines relationship

--- app/models/varietal.rb
+++ app/models/varietal.rb
@@ -1,5 +1,6 @@
 class Varietal < ApplicationRecord
   has_many :wines
+  has_many :countries, through: :wines
 
   validates :varietal_name, presence: true
   validates :varietal_name, uniqueness: true
