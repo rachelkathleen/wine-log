ff565c4c1f2c31b2ba3a8a0b827b869b1a17c12e
rachelkathleen <rachel.hawa@gmail.com>
2019-05-27 14:25:05 -0500

    sample image

--- app/helpers/wines_helper.rb
+++ app/helpers/wines_helper.rb
@@ -2,4 +2,12 @@ module WinesHelper
   def wine_seacher_link
     @link = "https://www.wine-searcher.com/find/#{@wine.producer}+#{@wine.wine_name}+#{@wine.vintage}"
   end
+
+  def show_image
+    if @wine.picture.attached?
+      "image_tag(@wine.picture, width: 200)"
+    else
+      "https://www.bruniglass.com/assets/Uploads/products/14973.jpg"
+    end
+  end
 end
