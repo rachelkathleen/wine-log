954d8adc935550cf5533658c702839cbed208561
rachelkathleen <rachel.hawa@gmail.com>
2019-05-29 14:46:05 -0500

    add scope methods

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -16,6 +16,22 @@ class Wine < ApplicationRecord
   validates :rating, numericality: { less_than_or_equal_to: 10, greater_than: 0,  only_integer: true }
 
   scope :is_favorite, -> {where(favorite: true)}
+  scope :order_by_rating, -> {order(:rating)}
+  scope :highly_rated, -> {where("rating > ?", 7)}
+  scope :red, -> {where(wine_type: "Red")}
+  scope :white, -> {where(wine_type: "White")}
+  scope :rose, -> {where(wine_type: "Rose")}
+  scope :sweet, -> {where(wine_type: "Sweet")}
+  scope :sparking, -> {where(wine_type: "Spakling")}
+  scope :other, -> {where(wine_type: "other")}
+
+
+  def self.fave_aromas
+    wine_ids = Wine.is_favorite.pluck(:id) & Wine.highly_rated.pluck(:id)
+    aroma_ids = WineAroma.where('wine_id IN (?)', wine_ids).pluck(:aroma_id)
+    aromas = Aroma.where('id in (?)', aroma_ids)
+    aromas.group(:id).order('COUNT(id) desc').limit(3).pluck(:aroma_name)
+  end
 
   def  varietal_attributes=(varietal_attributes)
     if varietal_attributes[:varietal_name].present?
