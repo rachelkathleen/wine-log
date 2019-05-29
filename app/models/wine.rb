7158d5c02fa5b668bfdefcfdcdf41730b609f6ba
rachelkathleen <rachel.hawa@gmail.com>
2019-05-29 17:55:22 -0500

    add class methods for most bottles and highest avg rating

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -22,15 +22,46 @@ class Wine < ApplicationRecord
   scope :white, -> {where(wine_type: "White")}
   scope :rose, -> {where(wine_type: "Rose")}
   scope :sweet, -> {where(wine_type: "Sweet")}
-  scope :sparking, -> {where(wine_type: "Spakling")}
+  scope :sparkling, -> {where(wine_type: "Sparkling")}
   scope :other, -> {where(wine_type: "other")}
 
+  def self.average_rating
+    average(:rating)
+  end
 
-  def self.fave_aromas
+  def self.top_rated
+    #want to return the wine type (white, red) with the highest average rating
+    averages = {"Red" => Wine.red.average_rating.to_f,
+                "White" => Wine.white.average_rating.to_f,
+                "Rose"=> Wine.rose.average_rating.to_f,
+                "Sparkling"=> Wine.sparkling.average_rating.to_f,
+                "sweet"=> Wine.sweet.average_rating.to_f}
+    top_type = averages.sort_by { |wine_type, avg| avg }.last[0]
+    # top_type[0]
+  end
+
+  def self.most_bottles
+    #want to return the wine type (white, red) with the highest count
+    count = {"Red"=> Wine.red.count,
+             "White"=> Wine.count,
+             "Rose"=> Wine.rose.count,
+             "Sparkling"=> Wine.sparkling.count,
+              "Sweet"=>Wine.sweet.count}
+    count.sort_by { |wine_type, count| count }.last[0]
+  end
+
+  def self.top_aromas
     wine_ids = Wine.is_favorite.pluck(:id) & Wine.highly_rated.pluck(:id)
     aroma_ids = WineAroma.where('wine_id IN (?)', wine_ids).pluck(:aroma_id)
     aromas = Aroma.where('id in (?)', aroma_ids)
     aromas.group(:id).order('COUNT(id) desc').limit(3).pluck(:aroma_name)
+  end
+
+  def self.top_terms
+    wine_ids = Wine.is_favorite.pluck(:id) & Wine.highly_rated.pluck(:id)
+    tasting_term_ids = WineTastingTerm.where('wine_id IN (?)', wine_ids).pluck(:tasting_term_id)
+    terms = TastingTerm.where('id in (?)', tasting_term_ids)
+    terms.group(:id).order('COUNT(id) desc').limit(3).pluck(:term)
   end
 
   def  varietal_attributes=(varietal_attributes)
@@ -50,6 +81,5 @@ class Wine < ApplicationRecord
   def self.wine_search(search)
     self.where('producer ILIKE :search or wine_name ILIKE :search or notes ILIKE :search or type ILIKE :search', search: "%#{search}%")
   end
-
 
 end
