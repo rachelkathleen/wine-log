298c086e64c103fb2b67ac751b2f451d91286e4e
rachelkathleen <rachel.hawa@gmail.com>
2019-05-29 01:36:56 -0500

    comment out unused code

--- app/controllers/wine_aromas_controller.rb
+++ app/controllers/wine_aromas_controller.rb
@@ -1,22 +1,22 @@
-class WineAromasController < ApplicationController
-  def new
-  end
-
-  def create
-    binding.pry
-    if params[:aroma_ids]
-      params[:aroma_ids].each do |aroma_id|
-        wine_id = params[:wine_id]
-        @winearoma = WineAroma.create(wine_id: wine_id, aroma_id: aroma_id)
-      end
-      redirect '/home'
-    else
-      @winearoma = WineAroma.create(params)
-      if @userpark.save
-          redirect "/home"
-        else
-          flash[:error] = "Sorry, there is a problem"
-        end
-      end
-    end
-end
+# class WineAromasController < ApplicationController
+#   def new
+#   end
+#
+#   def create
+#     binding.pry
+#     if params[:aroma_ids]
+#       params[:aroma_ids].each do |aroma_id|
+#         wine_id = params[:wine_id]
+#         @winearoma = WineAroma.create(wine_id: wine_id, aroma_id: aroma_id)
+#       end
+#       redirect '/home'
+#     else
+#       @winearoma = WineAroma.create(params)
+#       if @userpark.save
+#           redirect "/home"
+#         else
+#           flash[:error] = "Sorry, there is a problem"
+#         end
+#       end
+#     end
+# end
