43e4f29b10396a45f4fc6c45afd13fc53e481206
rachelkathleen <rachel.hawa@gmail.com>
2019-05-29 23:29:13 -0500

    add method for displaying insights

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,23 +1,11 @@
 module UsersHelper
 
-
-    def top_wine_type_rating
-      red = @user.wines.red.average_rating,
-      white = @user.wines.white.average_rating,
-      rose = @user.wines.rose.average_rating,
-      sparkling = @user.wines.sparkling.average_rating,
-      sweet =  @user.wines.sweet.average_rating
-      ratings = [red, white, sparkling, sweet, rose]
-
+  def insights
+    @user = current_user
+    if @user.wines.average_rating && @user.wines.top_aromas && @user.wines.top_terms
+      render 'insights'
+    else
+      render 'no_insights'
     end
-
-    # def top_wine_type_count(user)
-    #   # @user = current_user
-    #   count = [user.wines.red.count,
-    #               user.wines.white.count,
-    #               user.wines.rose.count,
-    #               user.wines.sparkling.count,
-    #               user.wines.sweet.count]
-    #   count.order('desc').limit(1)
-    # end
+  end
 end
