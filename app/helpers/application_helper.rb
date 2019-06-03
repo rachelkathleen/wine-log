ea1f75a76f058afa8644c56fc905cd052fa73948
rachelkathleen <rachel.hawa@gmail.com>
2019-06-03 00:05:14 -0500

    add helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,5 +12,30 @@ module ApplicationHelper
       flash[:error] = "You must be logged in to access this section"
       redirect_to login_path # halts request cycle
     end
-  end  
+  end
+
+  def scrape_grapes
+    require 'nokogiri'
+    require 'open-uri'
+    SITE = "https://www.jancisrobinson.com/learn/grape-varieties/"
+
+    red_page = Nokogiri::HTML(open(SITE+"red"))
+    white_page = Nokogiri::HTML(open(SITE+"white"))
+
+    grapes = []
+
+    red_nodeset = red_page.css(".info-table li a")
+    red_nodeset.each do |node|
+      grapes << node.attr("href").split("/").last.gsub("-", " ").capitalize
+    end
+
+    white_nodeset = white_page.css(".info-table li a")
+    white_nodeset.each do |node|
+      grapes << node.attr("href").split("/").last.gsub("-", " ").capitalize
+    end
+
+    grapes.uniq.each do |grape|
+      Varietal.create(varietal_name: grape)
+    end
+  end
 end
