ab978aa993b6171bf81615ebf524d3b94187ac48
rachelkathleen <rachel.hawa@gmail.com>
2019-06-04 18:12:30 -0500

    change for heroku deployment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+# end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook,'FACEBOOK_KEY', 'FACEBOOK_SECRET'
 end
