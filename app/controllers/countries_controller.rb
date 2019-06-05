343dd65db457ad70398ab928d6c8910049df4dae
rachelkathleen <rachel.hawa@gmail.com>
2019-06-05 14:07:36 -0500

    refactor search

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,9 +2,10 @@ class CountriesController < ApplicationController
   skip_before_action :require_login
 
   def index
-    @countries = Country.order(:country_name)
-    if params[:search]
-      @countries = Country.country_search(params[:search])
-    end
+    @countries = params[:search] ? Country.country_search(params[:search]) : Country.order(:country_name)
+    # @countries = Country.order(:country_name)
+    # if params[:search]
+    #   @countries = Country.country_search(params[:search])
+    # end
   end
 end
