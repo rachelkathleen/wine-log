a0c8a68f1cac2460a9de2b52634ea5f308dfd1e9
rachelkathleen <rachel.hawa@gmail.com>
2019-06-04 21:01:34 -0500

    debug fbook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-# end
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook,'FACEBOOK_KEY', 'FACEBOOK_SECRET'
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
+
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook,'FACEBOOK_KEY', 'FACEBOOK_SECRET'
+# end
