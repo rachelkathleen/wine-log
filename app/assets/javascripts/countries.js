24a2ead9ea02f8127eae77484074fb0fdf63e102
rachelkathleen <rachel.hawa@gmail.com>
2019-07-30 18:29:40 -0500

    fix syntax error

--- app/assets/javascripts/countries.js
+++ app/assets/javascripts/countries.js
@@ -12,18 +12,26 @@ class Country {
 }
 
 Country.prototype.formatHTML = function() {
-  return `<dt><a class="black-link"href="countries/${this.id}/wines">${this.country_name}</a></dt>`
+  return `<dt><a class="black-link country-link"href="countries/${this.id}/wines">${this.country_name}</a></dt>`
 }
 
 function listeningPageLoad() {
   $.get('/countries' + '.json', function(jsonData) {
     jsonData.forEach(function(data) {
       const countryData = new Country(data)
-
       const formatHTML = countryData.formatHTML()
       const listDiv = document.getElementById('list')
       listDiv.innerHTML += formatHTML
-      // moreClick()
-    })
-  });
-}
+    });//a new event listener for the click
+    //prevent default on click
+  }).then(function(wines) {
+                        $("#wines").html(`
+                            <h1>number of wines: ${wines.length}
+                            <p>the wines are:
+                            ${wines.map(function(wine) {
+                                return `<p>${wine.wine_name}</p>`;
+                              })
+                              .join("")}
+                              `)
+    });
+  }
