f18f2ca4d8d6cd07bad0da484bea908dd91657f9
rachelkathleen <rachel.hawa@gmail.com>
2019-08-01 14:54:36 -0500

    fix issue with form posting twice

--- app/assets/javascripts/wines.js
+++ app/assets/javascripts/wines.js
@@ -1,53 +1,55 @@
 // New wine form submits dynamically, then shows success message with link to
 // view new object, or error messages if not created
-  $(function () {
+$(function() {
     $('form').submit(function(event) {
-      //prevent form from submitting the default way
-      event.preventDefault();
+        //prevent form from submitting the default way
+        event.preventDefault();
 
-      var formData = new FormData($(this)[0]);
-     //  debugger
+        // Get the messages div.
 
-      $.ajax({
-         url: "/wines",
-         type: 'POST',
-         data: formData,
-         async: true,
-         cache: false,
-         contentType: false,
-         enctype: 'multipart/form-data',
-         processData: false,
-         success: function (response) {
-           alert(response);
-         }
-     });
-      // Get the messages div.
+        const formMessages = $('#form-messages');
+        $(formMessages).removeClass('alert-success')
+        $(formMessages).removeClass('alert-danger')
+        $(formMessages).html("<ul></ul>")
 
-      const formMessages = $('#form-messages');
-      $(formMessages).removeClass('alert-success')
-      $(formMessages).removeClass('alert-danger')
-      $(formMessages).html("<ul></ul>")
+        var formData = new FormData($(this)[0]);
+        //  debugger
 
+        $.ajax({
+            url: "/wines",
+            type: 'POST',
+            data: formData,
+            async: true,
+            cache: false,
+            contentType: false,
+            enctype: 'multipart/form-data',
+            processData: false,
+        }).then(function(data)
 
-      const values = $(this).serialize();
-      const posting = $.post('/wines', values);
-      posting.done(function(data) {
-      const wine = data;
-      if (!data.errors) {
-        $(formMessages).addClass('alert-success');
-        $(formMessages).show();
-        document.querySelector('#form-messages').innerHTML+=`<p>${wine.producer} - ${wine.wine_name} has been added to your cellar - click <a class="alert-link" href="/wines/${wine.id}">here</a> to view. </p>`
-        $("#producer").text(wine["producer"]);
-        $("#wineName").text(wine["wine_name"]);
-        $('form')[0].reset();
-      } else {
-        $(formMessages).addClass('alert-danger');
-        $(formMessages).show();
+            {
+                const wine = data;
+                if (!data.errors) {
+                    $(formMessages).addClass(
+                        'alert-success');
+                    $(formMessages).show();
+                    document.querySelector('#form-messages')
+                        .innerHTML +=
+                        `<p>${wine.producer} - ${wine.wine_name} has been added to your cellar - click <a class="alert-link" href="/wines/${wine.id}">here</a> to view. </p>`
+                    $("#producer").text(wine["producer"]);
+                    $("#wineName").text(wine["wine_name"]);
+                    $('form')[0].reset();
+                } else {
+                    $(formMessages).addClass(
+                        'alert-danger');
+                    $(formMessages).show();
 
-        data.errors.forEach(function(error){
-          document.querySelector('#form-messages').innerHTML+=`<p>${error}</p>`
-        })
-      }
-      });
+                    data.errors.forEach(function(error) {
+                        document.querySelector(
+                                '#form-messages')
+                            .innerHTML +=
+                            `<p>${error}</p>`
+                    })
+                }
+            });
     });
-  })
+})
