7446f2e7e5a9db25df894b1f89e6fab9a699f045
rachelkathleen <rachel.hawa@gmail.com>
2019-08-14 21:33:45 -0500

    start pagination

--- app/assets/javascripts/wines.js
+++ app/assets/javascripts/wines.js
@@ -4,6 +4,9 @@ $(function() {
     homePageLoad()
 })
 class Wine {
+
+  static all = []
+
     constructor(json) {
         this.id = json.id;
         this.producer = json.producer;
@@ -19,6 +22,7 @@ class Wine {
         this.producer = json.producer;
         this.wine_aromas = json.wine_aromas;
         this.wine_tasting_terms = json.wine_tasting_terms
+        Wine.all.push(this)
     }
 }
 
@@ -84,6 +88,110 @@ function wineModal(wine) {
           		})
           	});
           }
+
+            (function() {
+              "use strict";
+
+              function Pagination() {
+                console.log("pagination")
+
+                 const objJson = fetch(`/wines.json`);
+
+                const prevButton = document.getElementById('button_prev');
+                const nextButton = document.getElementById('button_next');
+                const clickPageNumber = document.querySelectorAll('.clickPageNumber');
+
+                let current_page = 1;
+                let records_per_page = 5;
+
+                this.init = function() {
+                    changePage(1);
+                    pageNumbers();
+                    selectedPage();
+                    clickPage();
+                    addEventListeners();
+               }
+
+                let addEventListeners = function() {
+                    prevButton.addEventListener('click', prevPage);
+                    nextButton.addEventListener('click', nextPage);
+                }
+
+                let selectedPage = function() {
+                    let page_number = document.getElementById('page_number').getElementsByClassName('clickPageNumber');
+                    for (let i = 0; i < page_number.length; i++) {
+                        if (i == current_page - 1) {
+                            page_number[i].style.opacity = "1.0";
+                        }
+                        else {
+                            page_number[i].style.opacity = "0.5";
+                        }
+                    }
+                }
+
+                let checkButtonOpacity = function() {
+                  current_page == 1 ? prevButton.classList.add('opacity') : prevButton.classList.remove('opacity');
+                  current_page == numPages() ? nextButton.classList.add('opacity') : nextButton.classList.remove('opacity');
+                }
+
+                let changePage = function(page) {
+                    const wineJS = document.getElementById('wine-js');
+
+                    if (page < 1) {
+                        page = 1;
+                    }
+                    if (page > (numPages() -1)) {
+                        page = numPages();
+                    }
+
+                    wineJS.innerHTML = "";
+
+                    for(var i = (page -1) * records_per_page; i < (page * records_per_page) && i < objJson.length; i++) {
+                        wineJS.innerHTML += tableHTML;
+                    }
+                    checkButtonOpacity();
+                    selectedPage();
+                }
+
+                let prevPage = function() {
+                    if(current_page > 1) {
+                        current_page--;
+                        changePage(current_page);
+                    }
+                }
+
+                let nextPage = function() {
+                    if(current_page < numPages()) {
+                        current_page++;
+                        changePage(current_page);
+                    }
+                }
+
+                let clickPage = function() {
+                    document.addEventListener('click', function(e) {
+                        if(e.target.nodeName == "SPAN" && e.target.classList.contains("clickPageNumber")) {
+                            current_page = e.target.textContent;
+                            changePage(current_page);
+                        }
+                    });
+                }
+
+                let pageNumbers = function() {
+                    let pageNumber = document.getElementById('page_number');
+                        pageNumber.innerHTML = "";
+
+                    for(let i = 1; i < numPages() + 1; i++) {
+                        pageNumber.innerHTML += "<span class='clickPageNumber'>" + i + "</span>";
+                    }
+                }
+
+                let numPages = function() {
+                    return Math.ceil(objJson.length / records_per_page);
+                }
+             }
+            let pagination = new Pagination();
+            pagination.init();
+          })();
 
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,4 @@
+require 'uglifier'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
